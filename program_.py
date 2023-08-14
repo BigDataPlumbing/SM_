a9e00a9165cd2753c5a113d4724af36d62b0254a
Big Data Plumbing <142255006+BigDataPlumbing@users.noreply.github.com>
2023-08-14 14:57:38 -0300

    program_.py

---
+++ program_.py
@@ -0,0 +1,37 @@
+                          if stl.startswith("A"):
+                                    # Data parameter from variable 'stl' is shown above (84)
+                                    #
+                                    #
+                                    # Next the program displays current step, data processing for this part may start subsequently (85)
+                                        print("Site 1 to be sourced to Client 1")
+                                        # (85)
+                                        #
+                                        """
+
+
+                                        |---                                                            ---|
+                                        |       Data processing subroutine is implemented here (86)        |
+                                        |                                                                  |
+                                        |       Code for data processing mast be placed here               |
+                                        |                                                                  |
+                                        |---                                                            ---|
+                
+                                                 
+
+                                        """
+                                        #
+                                        # Parameter for first data prompt at this stage confirmation is set next (87)
+                                        b = 1
+                                        # Above is the parameter for current data processing confirmation and sequencing (87)
+                                        #
+                                        #  
+                                        #
+                                        # Display from this segment confirmation is generated below (88)
+                                        print("  ")
+                                        # (88)
+                                        #
+                                        # Conclusion of this segment follows
+                                        # Continuation of classification follows from this part of data segmentation to the next one and so on (89)
+                                        continue
+                                        # Above is the step for next part (89)
+                                        #

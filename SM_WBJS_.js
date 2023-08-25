7e3a31a3e482b09a8baef227c7673483794827e4
Big Data Plumbing <142255006+BigDataPlumbing@users.noreply.github.com>
2023-08-25 12:17:25 -0300

    SM_WBJS_.js

---
+++ SM_WBJS_.js
@@ -0,0 +1,77 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+<meta charset="UTF-8">
+
+<!-- Parts of code should be placed hear via aditional files and following scripts -->
+<script defer src="/dn.js"></script>
+    
+<script type="text/javascript">
+    // 
+    // Prep the table
+    // "Data"
+function InsertGText () {
+    // Places the text in grid on the table at file =
+
+  }
+
+// Initialize Web App
+function InitSCM()
+{
+
+    //
+    // Subroutine for Web App opening goes here
+    //
+
+}
+
+// Display presentation
+function Display()
+{
+
+    //
+    // Interactive Init of UI subroutine
+    //
+
+}
+
+// Parts of code should be placed hear and via aditional files    
+    
+</script>
+</head>
+
+<body onload="InsertGText();">
+
+    <!-- Initial table for input data -->
+    <p>Table with Data</p>
+    <table id="tbl1"> 
+          <!-- Table Area for the Data -->
+    </table>
+
+    <!-- Initial table for output data -->
+    <table> 
+    </table>
+
+  <!-- Storage unload to Web App -->
+    
+  <script>
+    // Parts of code should be placed hear and via aditional files
+
+      </script>
+
+    <input
+     type="button"
+     value="Init"
+      onclick="InitSCM()" />
+
+    <input
+     type="button"
+     value="Display Data"
+      onclick="Display()" />
+    
+  <!-- Parts of code should be placed hear via aditional files -->
+  <script src="./dindex.js"></script>
+         
+</body>
+</html>
